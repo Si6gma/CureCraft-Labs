fb154a26e12070cb07f5817f841d2256d837470c
Si6gma <neiv.malaviya@gmail.com>
2025-12-27 15:47:24 +0100

    Testing input

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,27 @@
-#include "math.h"
 #include <iostream>
 
-int main() {
+#include "math.h"
+#include "input.h"
+
+int main()
+{
     int a = 5;
     int b = 120;
     int sum = add(a, b);
     std::cout << "The sum of " << a << " and " << b << " is: " << sum << std::endl;
+
+    std::cout << "Press keys (q to quit)\n";
+
+    while (true)
+    {
+        char c = read_keypress();
+
+        std::cout << "Key: '" << c
+                  << "' ASCII: " << static_cast<int>(c) << "\n";
+
+        if (c == 'q')
+            break;
+    }
+
     return 0;
 }
