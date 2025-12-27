51a0de9940d17a8579b2c52276b2ab06bc0fef70
Si6gma <neiv.malaviya@gmail.com>
2025-12-27 15:21:53 +0100

    Testing auto pull

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 int main() {
     int a = 5;
-    int b = 10;
+    int b = 120;
     int sum = add(a, b);
     std::cout << "The sum of " << a << " and " << b << " is: " << sum << std::endl;
     return 0;
