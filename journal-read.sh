c91d0fef9f9fb7325fbc0956434f0fef4794ca38
Si6gma <neiv.malaviya@gmail.com>
2025-12-27 16:06:16 +0100

    Made jctl read only curecraft service

--- journal-read.sh
+++ journal-read.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-SERVICE="${1:-}"
+# SERVICE="${1:-}"
 
-if [[ -n "$SERVICE" ]]; then
-  echo "Following logs for service: $SERVICE"
-  sudo journalctl -u "$SERVICE" -f
-else
-  echo "Following all journal logs"
-  sudo journalctl -f
-fi
+  echo "Following logs for service: curecraft"
+  sudo journalctl -u curecraft -f
