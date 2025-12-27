ac8990b74b0d98f532d47d3e9435a441d6cf1e82
Si6gma <90695450+Si6gma@users.noreply.github.com>
2025-12-27 15:59:26 +0100

    Removed the inputs

--- src/main.cpp
+++ src/main.cpp
@@ -10,24 +10,5 @@ int main()
     int sum = add(a, b);
     std::cout << "The sum of " << a << " and " << b << " is: " << sum << std::endl;
 
-    std::cout << "Press keys (q to quit)\n";
-
-    while (true)
-    {
-        char c = read_keypress();
-        unsigned char uc = static_cast<unsigned char>(c);
-
-        if (uc >= 32 && uc <= 126)
-        { // printable ASCII
-            std::cout << "Key: '" << c << "'  code=" << (int)uc << "\n";
-        }
-        else
-        {
-            std::cout << "Key: (non-printable) code=" << (int)uc << "\n";
-        }
-        if (c == 'q')
-            break;
-    }
-
     return 0;
 }
