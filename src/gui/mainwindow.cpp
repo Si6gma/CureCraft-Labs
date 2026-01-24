a340766b6e0f1068b6ef0b44b49cc0330ab6bda5
Si6gma <90695450+Si6gma@users.noreply.github.com>
2026-01-24 14:45:22 +0100

    Optmised compliation and combined .md files

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -3,7 +3,6 @@
 
 #include <cmath>
 #include <QtGlobal>
-#include <algorithm>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow)
@@ -11,13 +10,13 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     setupPlots();
 
-    // Reserve space to prevent reallocations (deque pre-allocates)
-    xECG_.resize(maxPoints_);
-    yECG_.resize(maxPoints_);
-    xSpO2_.resize(maxPoints_);
-    ySpO2_.resize(maxPoints_);
-    xResp_.resize(maxPoints_);
-    yResp_.resize(maxPoints_);
+    // Reserve space in QVector to prevent reallocations (critical for performance)
+    xECG_.reserve(maxPoints_);
+    yECG_.reserve(maxPoints_);
+    xSpO2_.reserve(maxPoints_);
+    ySpO2_.reserve(maxPoints_);
+    xResp_.reserve(maxPoints_);
+    yResp_.reserve(maxPoints_);
 
     // 50 ms = 20 Hz GUI update. Optimized for Raspberry Pi.
     timer_.setInterval(50);
@@ -85,17 +84,18 @@ void MainWindow::setupPlots()
     ui->plotResp->xAxis->setRange(0, windowSeconds_);
 }
 
-void MainWindow::pushSample(std::deque<double> &x, std::deque<double> &y, double t, double v)
+void MainWindow::pushSample(QVector<double> &x, QVector<double> &y, double t, double v)
 {
-    // Deque push_back is O(1), pop_front is O(1) (unlike QVector)
+    // Add new samples
     x.push_back(t);
     y.push_back(v);
 
-    // Remove oldest samples efficiently
-    while (x.size() > static_cast<size_t>(maxPoints_))
+    // Remove oldest samples efficiently when exceeding max points
+    if (x.size() > maxPoints_)
     {
-        x.pop_front();
-        y.pop_front();
+        const int toRemove = x.size() - maxPoints_;
+        x.remove(0, toRemove);
+        y.remove(0, toRemove);
     }
 }
 
@@ -113,7 +113,7 @@ void MainWindow::onTick()
     const double spo2 = waveParams_.spO2Base + waveParams_.spO2Amplitude * std::sin(2.0 * M_PI * waveParams_.spO2Freq * t_);
     const double resp = waveParams_.respAmplitude * std::sin(2.0 * M_PI * waveParams_.respFreq * t_);
 
-    // Add samples to circular buffers (O(1) with deque)
+    // Add samples to circular buffers
     pushSample(xECG_, yECG_, t_, ecg);
     pushSample(xSpO2_, ySpO2_, t_, spo2);
     pushSample(xResp_, yResp_, t_, resp);
@@ -132,28 +132,22 @@ void MainWindow::onTick()
     const double left = t_ - windowSeconds_;
     const double right = t_;
 
-    // Update visible plots efficiently (convert deque to QVector for plotting)
+    // Update visible plots efficiently - QVector is native to QCustomPlot (no conversion!)
     if (ecgVisible)
     {
-        QVector<double> xVec(xECG_.begin(), xECG_.end());
-        QVector<double> yVec(yECG_.begin(), yECG_.end());
-        ui->plotECG->graph(0)->setData(xVec, yVec);
+        ui->plotECG->graph(0)->setData(xECG_, yECG_, true); // true = already sorted
         ui->plotECG->xAxis->setRange(left, right);
         ui->plotECG->replot(QCustomPlot::rpQueuedReplot);
     }
     if (spo2Visible)
     {
-        QVector<double> xVec(xSpO2_.begin(), xSpO2_.end());
-        QVector<double> yVec(ySpO2_.begin(), ySpO2_.end());
-        ui->plotSpO2->graph(0)->setData(xVec, yVec);
+        ui->plotSpO2->graph(0)->setData(xSpO2_, ySpO2_, true);
         ui->plotSpO2->xAxis->setRange(left, right);
         ui->plotSpO2->replot(QCustomPlot::rpQueuedReplot);
     }
     if (respVisible)
     {
-        QVector<double> xVec(xResp_.begin(), xResp_.end());
-        QVector<double> yVec(yResp_.begin(), yResp_.end());
-        ui->plotResp->graph(0)->setData(xVec, yVec);
+        ui->plotResp->graph(0)->setData(xResp_, yResp_, true);
         ui->plotResp->xAxis->setRange(left, right);
         ui->plotResp->replot(QCustomPlot::rpQueuedReplot);
     }
